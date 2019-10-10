37df44aaebf8f7650fa58f999248b14d00f25082
John Kenneth Garcia <kengarcia.96@gmail.com>
2019-10-10 12:12:24 +0800

    Added Mobile java

---
+++ src/main/java/Mobile.java
@@ -0,0 +1,11 @@
+public class Mobile {
+    private String name;
+    private String color;
+    private String brand;
+
+    public void call (String message){
+        System.out.println("Message : " + message);
+    }
+
+
+}
